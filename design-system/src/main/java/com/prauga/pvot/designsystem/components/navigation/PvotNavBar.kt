598513cce1f0529d30c48aa8d6bd7ef1f9ce14c8
Saalim Quadri <danascape@gmail.com>
2026-08-18 13:13:38 +0530

    design-system: Do not cache the text width
    - fixes issue with clips in text while using downloadable fonts
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- design-system/src/main/java/com/prauga/pvot/designsystem/components/navigation/PvotNavBar.kt
+++ design-system/src/main/java/com/prauga/pvot/designsystem/components/navigation/PvotNavBar.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -118,9 +117,7 @@ fun calculateExpandedWidth(labelText: String, sizes: PvotNavBarSizes): Dp {
         fontWeight = FontWeight.SemiBold,
         fontSize = sizes.labelFontSize
     )
-    val textLayoutResult = remember(labelText, textStyle) {
-        textMeasurer.measure(labelText, textStyle)
-    }
+    val textLayoutResult = textMeasurer.measure(labelText, textStyle)
 
     val density = LocalDensity.current
     val intrinsicWidth = with(density) {
